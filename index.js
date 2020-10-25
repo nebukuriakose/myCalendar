78d7e1556c7921e58cded55dbc3c20825e6600cc
nebukuriakose <64222197+nebukuriakose@users.noreply.github.com>
2020-10-24 22:04:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function onChatBotReady() {
-
-  // You have to define HTML meta "bs:input:buttonName" in order to inform bot send data to buttonName parameter webview
   var btnName = BotStarWebview('getParameter', 'buttonName');
-
   var btn = document.getElementById('btn');
   if (btnName) {
     btn.appendChild(document.createTextNode(btnName));
