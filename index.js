c238c487f9ac694e3a88cdd3f72f21cff3cf10ca
nebukuriakose <64222197+nebukuriakose@users.noreply.github.com>
2020-10-24 22:14:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function onChatBotReady() {
   var btnName = BotStarWebview('getParameter', 'buttonName');
+
   var btn = document.getElementById('btn');
   if (btnName) {
     btn.appendChild(document.createTextNode(btnName));
     btn.style.display = 'inline-block';
   }
-  
+
+}
+
 function sendResponse() {
   event.preventDefault();
   console.log("Button Click and your work")
@@ -13,6 +16,5 @@ function sendResponse() {
     BotStarWebview('sendResponse', "", {}, 'Button Clicked').catch((err) => {
       console.log(err);
     });
-  }, 0);
+  }, 2000);
 }
-
